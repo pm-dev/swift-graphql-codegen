912cf895f5dd45e823376742120f5cc28dd3a14f
Peter Meyers <meyers@openai.com>
2026-08-04 14:51:38 -0400

    Label UTF-16 source range subscripts

--- Sources/GraphQLCodegen/Helpers/String.swift
+++ Sources/GraphQLCodegen/Helpers/String.swift
@@ -1,8 +1,12 @@
 extension String {
-    subscript(_ range: Range<Int>) -> Substring {
+    subscript(utf16Range range: Range<Int>) -> Substring {
         self[
             String.Index(utf16Offset: range.lowerBound, in: self)..<
             String.Index(utf16Offset: range.upperBound, in: self)
         ]
+    }
+
+    var capitalizedFirst: String {
+        prefix(1).capitalized + dropFirst()
     }
 }
