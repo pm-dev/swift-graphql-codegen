eefcb26a9f3bd928c9d2e264bd5091b070b9fc17
Peter Meyers <petermeyers1@gmail.com>
2026-08-04 14:45:14 -0400

    Refactor String extension to simplify substring handling
    Removed the InvalidUTF16Range error handling and substring method. Added a subscript for extracting substrings using UTF-16 ranges.

--- Sources/GraphQLCodegen/Helpers/String.swift
+++ Sources/GraphQLCodegen/Helpers/String.swift
@@ -1,34 +1,8 @@
 extension String {
-    struct InvalidUTF16Range: Error, CustomStringConvertible {
-        let range: Range<Int>
-
-        var description: String {
-            "The UTF-16 range \(range) does not identify valid String boundaries."
-        }
-    }
-
-    func substring(utf16Range range: Range<Int>) throws -> Substring {
-        let utf16 = utf16
-        guard range.lowerBound >= 0,
-              range.upperBound >= range.lowerBound,
-              let utf16Start = utf16.index(
-                  utf16.startIndex,
-                  offsetBy: range.lowerBound,
-                  limitedBy: utf16.endIndex
-              ),
-              let utf16End = utf16.index(
-                  utf16.startIndex,
-                  offsetBy: range.upperBound,
-                  limitedBy: utf16.endIndex
-              ),
-              let start = String.Index(utf16Start, within: self),
-              let end = String.Index(utf16End, within: self) else {
-            throw InvalidUTF16Range(range: range)
-        }
-        return self[start..<end]
-    }
-
-    var capitalizedFirst: String {
-        prefix(1).capitalized + dropFirst()
+    subscript(_ range: Range<Int>) -> Substring {
+        self[
+            String.Index(utf16Offset: range.lowerBound, in: self)..<
+            String.Index(utf16Offset: range.upperBound, in: self)
+        ]
     }
 }
