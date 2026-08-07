806c36f8ff6d103455f8cf53bfc0c485a681904c
Peter Meyers <peter@Peters-MacBook-Air.local>
2026-08-07 19:19:30 -0400

    Separate examples from package products

---
+++ Examples/StarwarsExample/Package.swift
@@ -0,0 +1,26 @@
+// swift-tools-version: 6.3
+import PackageDescription
+
+let package = Package(
+    name: "StarwarsExample",
+    platforms: [
+        .macOS(.v14),
+    ],
+    dependencies: [
+        .package(path: "../.."),
+    ],
+    targets: [
+        .executableTarget(
+            name: "StarwarsExample",
+            dependencies: [
+                .product(name: "GraphQLCodegen", package: "swift-graphql-codegen"),
+            ],
+            exclude: [
+                "Operations/FavoriteEpisodeChangedSubscription.graphql",
+                "Operations/HeroQuery.graphql",
+                "Operations/SetFavoriteEpisodeMutation.graphql",
+                "schema.sdl",
+            ]
+        ),
+    ]
+)
